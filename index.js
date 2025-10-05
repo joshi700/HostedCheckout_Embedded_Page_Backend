99f36521a6b17e3e43b40970e03eb298c9ee21df
GJ <joshi700@gmail.com>
2025-10-05 20:54:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,62 @@ app.get('/favicon.ico', (req, res) => {
   res.status(204).end();
 });
 
+app.post('/', async (req, res) => {
+  try {
+    const trxid = crypto.randomBytes(8).toString("hex");
+    const orderid = crypto.randomBytes(8).toString("hex");
+    console.log("The orderid is " + orderid);
+    console.log("The trxid is " + trxid);
+    console.log("The password is " + process.env.MASTERCARD_AUTH_TOKEN);
+    console.log("The API Version is " + process.env.API_VERSION);
+    
+    const postData = {
+      "apiOperation": "INITIATE_CHECKOUT",
+      "checkoutMode": "WEBSITE",
+      "interaction": {
+        "operation": "PURCHASE",
+        "displayControl": {
+            "billingAddress": "HIDE"
+        },
+        "merchant": { 
+          "name": process.env.MERCHANT_NAME || "ABC Enterprises LLC",
+          "url": process.env.MERCHANT_URL || "https://mastercard.com"
+        },
+        "returnUrl": process.env.RETURN_URL || "https://hosted-checkout-embedded-page.vercel.app/ReceiptPage"
+      },
+      "order": {
+        "currency": process.env.CURRENCY || "USD",
+        "amount": process.env.DEFAULT_AMOUNT || "99.00",
+        "id": orderid,
+        "description": process.env.ORDER_DESCRIPTION || "Goods and Services"
+      }
+    };
+
+    const axiosConfig = {
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        "Access-Control-Allow-Origin": "*",
+        'Authorization': `Basic ${process.env.MASTERCARD_AUTH_TOKEN}`,
+        "Accept": "application/json"
+      }
+    };
+
+    const apiUrl = `${process.env.MASTERCARD_API_BASE_URL}/api/rest/version/${process.env.API_VERSION}/merchant/${process.env.MERCHANT_ID}/session`;
+    const response = await axios.post(apiUrl, postData, axiosConfig);
+    
+    console.log("RESPONSE RECEIVED Create: ", response.data.session.id);
+    const sessionId = response.data.session.id;
+    console.log("The orderid is " + orderid);
+    console.log("The trxid is " + trxid);
+    console.log("The password is " + process.env.MASTERCARD_AUTH_TOKEN);
+    console.log("The API Version is " + process.env.API_VERSION);
+    console.log("The sessionId in backend is " + sessionId);
+    res.send(sessionId);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
 
 
 app.listen(port, () => {
